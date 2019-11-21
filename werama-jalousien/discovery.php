d48e4b4f7df781ca1f4c7aac6dd84d919d02fcd6
Volker Christian <volker.christian@fh-hagenberg.at>
2019-11-21 19:00:51 +0100

    Polish

--- werama-jalousien/discovery.php
+++ werama-jalousien/discovery.php
@@ -26,10 +26,9 @@ else
             "header" => "Authorization: Bearer " . $alexa_discovery->payload->token . "\r\n"
         ]
     ];
-
     $context = stream_context_create($opts);
-
     $oauth_user = file_get_contents('https://cloud.vchrist.at/ocs/v2.php/cloud/user?format=json', false, $context);
+
     $fp = fopen(dirname(__FILE__).'/user.json', 'w');
     fwrite($fp, $oauth_user);
     fclose($fp);
