f0a97f48aa3ea5391a523a133a9f707947739ed3
Volker Christian <volker.christian@fh-hagenberg.at>
2019-11-22 13:32:19 +0100

    Fix

--- werama-jalousien/discovery.php
+++ werama-jalousien/discovery.php
@@ -27,6 +27,7 @@ else
     ];
     $context = stream_context_create($opts);
     $oauth_user = file_get_contents('https://cloud.vchrist.at/ocs/v2.php/cloud/user?format=json', false, $context);
+    $oauth_user_data = json_decode($oauth_user);
     
     $user_check_faild = $oauth_user_data->ocs->meta->message != "OK" || !in_array("Amazon", $oauth_user_data->ocs->data->groups);
     
