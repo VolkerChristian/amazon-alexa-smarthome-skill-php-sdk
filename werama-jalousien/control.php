fb8c75dda804c0aab98f1351f9d4bc0107df7501
Volker Christian <volker.christian@fh-hagenberg.at>
2020-01-13 09:32:14 +0100

    Removed code to connect via ssh

--- werama-jalousien/control.php
+++ werama-jalousien/control.php
@@ -60,23 +60,6 @@ else
                 {
                     if (isset($json->jalousien->$endpoint->$alexa_control_todo->$alexa_control_payload_mode))
                     {
-                        /*
-                        $connection = ssh2_connect($json->jalousien->$endpoint->$alexa_control_todo->$alexa_control_payload_mode->host, 22);
-                        ssh2_auth_pubkey_file($connection, 'pi', '/var/www/.ssh/id_rsa.pub', '/var/www/.ssh/id_rsa', '');
-                        if ( $connection != FALSE ) {
-                            $contextProperty = new AlexaContextProperty("Alexa.ModeController", "mode", $alexa_control_payload_mode, 500);
-                            $contextProperty->instance = $alexa_control->header->instance;
-                            $context = new AlexaContext();
-                            $context->add_property($contextProperty);
-                            $state = new AlexaAsyncResponse($context, $alexa_control->scope()->token, $endpoint, $alexa_control->correlationToken());
-                            ssh2_exec($connection, $json->jalousien->$endpoint->$alexa_control_todo->$alexa_control_payload_mode->command);
-                        } else {
-                            $err = new AlexaError(AlexaErrorTypes::ENDPOINT_UNREACHABLE);
-                            $state = new AlexaErrorResponse($alexa_control->endpoint->endpointId, $err->type, $err->msg);
-                        }
-                        ssh2_disconnect ($connection);
-                        unset($connection);
-                        */
                         $result = file_get_contents($json->jalousien->$endpoint->$alexa_control_todo->$alexa_control_payload_mode->url);
                         if (strpos($result, "OK:") !== false) {
 						    $contextProperty = new AlexaContextProperty("Alexa.ModeController", "mode", $alexa_control_payload_mode, 500);
